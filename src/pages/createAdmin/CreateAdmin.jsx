0016bdc6c271ad89703a6b4ed3afbd7692b53d8f
andreicruceanu <cruceanuandrei10@gmail.com>
2024-02-23 22:28:33 +0200

    add container content

--- src/pages/createAdmin/CreateAdmin.jsx
+++ src/pages/createAdmin/CreateAdmin.jsx
@@ -1,5 +1,6 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import React, { useState } from "react";
+import ContainerContent from "../../components/common/container/ContainerContent";
 
 import ButtonCostum from "../../components/common/Buttons/ButtonCostum";
 import InputCustom from "../../components/common/inputs/InputCustom";
@@ -64,112 +65,96 @@ const CreateAdmin = () => {
   };
 
   return (
-    <Box padding={4}>
-      <Typography variant="h4" sx={{ fontWeight: 600, mb: 4 }}>
-        Create New Admin
-      </Typography>
-      <Box
-        sx={{
-          backgroundColor: "white",
-          width: "100%",
-          height: "640px",
-          border: "0.3px solid #B9B9B9",
-          borderRadius: "14px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          component="form"
-          minWidth="750px"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <Stack flexDirection="row" gap={5} mb={5}>
-            <InputCustom
-              id="firstName"
-              type="text"
-              placeholder="Enter firstname"
-              label="First Name"
-              name="firstName"
-              required
-              register={register}
-              errors={errors}
-            />
-            <InputCustom
-              id="lastName"
-              name="lastName"
-              type="text"
-              placeholder="Enter Lastname"
-              label="Last Name"
-              register={register}
-              required
-              errors={errors}
-            />
-          </Stack>
+    <ContainerContent
+      title="Create New Admin
+   "
+    >
+      <Box component="form" minWidth="750px" onSubmit={handleSubmit(onSubmit)}>
+        <Stack flexDirection="row" gap={5} mb={5}>
+          <InputCustom
+            id="firstName"
+            type="text"
+            placeholder="Enter firstname"
+            label="First Name"
+            name="firstName"
+            required
+            register={register}
+            errors={errors}
+          />
+          <InputCustom
+            id="lastName"
+            name="lastName"
+            type="text"
+            placeholder="Enter Lastname"
+            label="Last Name"
+            register={register}
+            required
+            errors={errors}
+          />
+        </Stack>
 
-          <Stack flexDirection="row" gap={5} mb={5}>
-            <InputCustom
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Enter Email"
-              label="Email"
-              register={register}
-              required
-              errors={errors}
-            />
-            <InputCustom
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Enter UserName"
-              label="Username"
-              register={register}
-              required
-              errors={errors}
-            />
-          </Stack>
+        <Stack flexDirection="row" gap={5} mb={5}>
+          <InputCustom
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter Email"
+            label="Email"
+            register={register}
+            required
+            errors={errors}
+          />
+          <InputCustom
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter UserName"
+            label="Username"
+            register={register}
+            required
+            errors={errors}
+          />
+        </Stack>
 
-          <Stack flexDirection="row" gap={5}>
-            <InputCustom
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter Password"
-              label="Password"
-              required
-              register={register}
-              errors={errors}
-            />
-            <SelectCustom
-              type="select"
-              id="role"
-              label="Role"
-              required
-              {...register("role")}
-              errors={errors}
-            >
-              <option value="guest">Guest</option>
-              <option value="admin">Admin</option>
-              <option value="owner">Owner</option>
-            </SelectCustom>
-          </Stack>
-          <Stack mt={5} alignItems="center">
-            <ButtonCostum
-              type="submit"
-              size="large"
-              variant="contained"
-              loading={onRequest}
-              disabled={onRequest}
-              sx={{ width: "250px", padding: "15px 0px", borderRadius: "12px" }}
-            >
-              {onRequest ? "Loading..." : "Create Admin"}
-            </ButtonCostum>
-          </Stack>
-        </Box>
+        <Stack flexDirection="row" gap={5}>
+          <InputCustom
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter Password"
+            label="Password"
+            required
+            register={register}
+            errors={errors}
+          />
+          <SelectCustom
+            type="select"
+            id="role"
+            label="Role"
+            required
+            width="60%"
+            {...register("role")}
+            errors={errors}
+          >
+            <option value="guest">Guest</option>
+            <option value="admin">Admin</option>
+            <option value="owner">Owner</option>
+          </SelectCustom>
+        </Stack>
+        <Stack mt={5} alignItems="center">
+          <ButtonCostum
+            type="submit"
+            size="large"
+            variant="contained"
+            loading={onRequest}
+            disabled={onRequest}
+            sx={{ width: "250px", padding: "15px 0px", borderRadius: "12px" }}
+          >
+            {onRequest ? "Loading..." : "Create Admin"}
+          </ButtonCostum>
+        </Stack>
       </Box>
-    </Box>
+    </ContainerContent>
   );
 };
 
