cad0c5265e6af10b798540c9789c9e20e05c9e5c
andreicruceanu <cruceanuandrei10@gmail.com>
2024-03-18 00:54:27 +0200

    api admin and infoUserAndMovie

--- src/api/modules/admin.api.js
+++ src/api/modules/admin.api.js
@@ -2,6 +2,8 @@ import privateClient from "../client/private.client";
 
 const authEndpoints = {
   createAdmin: "admin/create",
+  deleteUser: (userId) => `admin/deleteUser/${userId}`,
+  updateUser: (userId) => `admin/updateProfile/${userId}`,
 };
 
 const adminApi = {
@@ -22,6 +24,31 @@ const adminApi = {
         password,
         role,
       });
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+  deleteUser: async (userId) => {
+    try {
+      const response = await privateClient.delete(
+        authEndpoints.deleteUser(userId)
+      );
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+  updateUser: async ({ userId, email, firstName, lastName }) => {
+    try {
+      const response = await privateClient.patch(
+        authEndpoints.updateUser(userId),
+        {
+          email,
+          firstName,
+          lastName,
+        }
+      );
       return { response };
     } catch (err) {
       return { err };
